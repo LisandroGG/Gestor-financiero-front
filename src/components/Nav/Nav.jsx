90112b05cca5fcd9aa4ebcfd800d1ec9eba1faf8
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2025-03-31 13:52:55 -0300

    nav logo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,8 +13,8 @@ const Nav = ({usuario, logout}) => {
         <header className="bg-nav p-4 rounded-b-2xl">
             <nav>
                 <ul className="flex justify-between text-white items-center md:mx-32 lg:mx-52">
-                    <li>Hola {usuario.nombreUsuario}</li>
-                    <li>Logo</li>
+                    <li>Hola <span className="text-sky-500 font-bold">{usuario.nombreUsuario}</span></li>
+                    <li><img src="assets/logo.svg" alt='logo' width="45px" /></li>
                     <li>
                         <button onClick={logout} className="p-2 rounded-lg bg-sky-500 ring-sky-700 hover:ring-2 hover:bg-sky-700 font-bold transition-all">
                             Cerrar sesión
